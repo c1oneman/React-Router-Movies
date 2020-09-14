a96f1788ea21fea2eab11b0cd556b115cc9855aa
BuildTools <clayloneman@gmail.com>
2020-09-14 15:41:58 -0500

    finished

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
+import { useRouteMatch } from 'react-router-dom'
 export default function MovieList(props) {
   return (
     <div className="movie-list">
-      {props.movies.map(movie => (
+      {
+      
+      props.movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
@@ -12,8 +15,10 @@ export default function MovieList(props) {
 
 function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
+  const { url, path } = useRouteMatch()
 
   return (
+    <Link to={`/movie/${props.movie.id}`}>
     <div className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
@@ -23,5 +28,7 @@ function MovieDetails(props) {
         Metascore: <strong>{metascore}</strong>
       </div>
     </div>
+    </Link>
+    
   );
 }
